c67a567a12ded57287167822777c491de28e913c
PeterCxy <xqsx43cxy@126.com>
2014-01-05 22:37:16 +0800

    ApplicationAdapter: clean up

--- src/us/shandian/mod/swipeback/adapter/ApplicationAdapter.java
+++ src/us/shandian/mod/swipeback/adapter/ApplicationAdapter.java
@@ -13,7 +13,10 @@ import android.content.pm.PackageManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 import us.shandian.mod.swipeback.R;
 
